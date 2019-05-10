a2a8edee9678a1141a6d9ea5088c27c2eb38d7d2
Stephen McGuckin <stephenmcguckin@Stephens-MacBook-Air.local>
2019-05-10 01:56:25 -0700

    added e2e test along with classes that wrap rack test

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
 source 'https://rubygems.org' do
   gem 'json'
+  gem 'pry'
   gem 'rspec', '~> 3.8'
   gem 'rack'
+  gem 'rack-test'
   gem 'rake', '~> 12.3'
   gem 'sinatra', '~> 2.0'
 end
--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -1,6 +1,58 @@
-describe  do
+require 'rack/test'
 
-  it 'does something' do
-    expect(true).to be_truthy
+class Request
+
+  def initialize(app)
+    @browser = Rack::Test::Session.new(Rack::MockSession.new(app))
+  end
+
+  def get(route)
+    Response.new(make_request method: :get, route: route)
+  end
+
+  private
+
+  def make_request(method:, route:)
+    @browser.send method, route
+  end
+end
+
+class Response
+  def initialize(response)
+    @response = response
+  end
+
+  def status
+    @response.status
+  end
+
+  def body
+    return JSON.parse(@response.body) if header['Content-Type'] == 'application/json'
+    @response.body
+  end
+
+  def header
+    @response.header
+  end
+end
+
+server_const = Server
+request = Request.new(server_const)
+
+describe server_const do
+
+  context 'GET /' do
+
+    before :all do
+      @result = request.get('/')
+    end
+
+    it 'responds with a 200 status' do
+      expect(@result.status).to eq 200
+    end
+
+    it 'responds with an ok health status' do
+      expect(@result.body).to eq({ 'health_check' => 'ok'})
+    end
   end
 end
